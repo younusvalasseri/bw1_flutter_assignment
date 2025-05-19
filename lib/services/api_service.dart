0c65f42d3ec66c23e5eecc220112e9b05e354d84
younusvalasseri <younusv@gmail.com>
2025-05-19 12:37:04 +0530

    api service exceptions

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import '../models/notification_model.dart';
@@ -8,16 +9,44 @@ class ApiService {
       'https://raw.githubusercontent.com/sayanp23/test-api/main/test-notifications.json';
 
   static Future<List<NotificationModel>> fetchNotifications() async {
-    final response = await http.get(Uri.parse(url));
+    try {
+      final response = await http.get(Uri.parse(url));
 
-    if (response.statusCode == 200) {
-      return compute(parseNotifications, response.body);
-    } else {
-      throw Exception('Failed to load notifications');
+      switch (response.statusCode) {
+        case 200:
+          return compute(parseNotifications, response.body);
+
+        case 400:
+          throw Exception('Bad request (400)');
+
+        case 401:
+          throw Exception('Unauthorized access (401)');
+
+        case 403:
+          throw Exception('Forbidden (403)');
+
+        case 404:
+          throw Exception('Data not found (404)');
+
+        case 500:
+          throw Exception('Server error (500)');
+
+        default:
+          throw Exception(
+            'Unexpected error: ${response.statusCode} - ${response.reasonPhrase}',
+          );
+      }
+    } on SocketException {
+      throw Exception('No Internet connection.');
+    } on HttpException {
+      throw Exception('Could not find the resource.');
+    } on FormatException {
+      throw Exception('Bad response format.');
+    } catch (e) {
+      throw Exception('Unexpected error: ${e.toString()}');
     }
   }
 
-  // Optional and now corrected
   static List<NotificationModel> parseNotifications(String responseBody) {
     final List<dynamic> parsed = json.decode(responseBody);
     return parsed
